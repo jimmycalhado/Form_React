fc9e3d2e52d99700d931933ed0035e8b44109274
jimmycalhado <jimmyandrey.1208@gmail.com>
2023-11-15 11:20:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// Função construtora para o formulário
+function Formulario() {
+  const [dados, setDados] = useState({
+    nome: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    setDados({
+      ...dados,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Dados enviados:', dados);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <span>
+        <label>
+          Nome:
+        </label>
+        <input
+          type="text"
+          name="nome"
+          value={dados.nome}
+          onChange={handleChange}
+        />
+      </span>
+      <span>
+        <label>
+          Telefone:
+        </label>
+        <input
+          type="tel"
+          name="telefone"
+          value={dados.telefone}
+          onChange={handleChange}
+        />
+        </span>
+        <span>
+        <label>
+          Aniversario:
+        </label>
+        <input
+          type="date"
+          name="data"
+          value={dados.tdata}
+          onChange={handleChange}
+        />
+      </span>
+      <span>
+        <label>
+          CPF:
+        </label>
+        <input
+          type="text"
+          name="cpf"
+          pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
+          title="Digite o CPF no formato nnn.nnn.nnn-nn"
+          value={dados.cpf}
+          onChange={handleChange}
+        />
+      </span>
+      <span>
+        <label>
+          Email:
+        </label>
+        <input
+          type="email"
+          name="email"
+          value={dados.email}
+          onChange={handleChange}
+        />
+      </span>
+      <span>
+        <label>
+          Senha:
+        </label>
+        <input
+          type="password"
+          name="senha"
+          value={dados.senha}
+          onChange={handleChange}
+        />
+      </span>
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Formulário em React</h1>
+      <Formulario />
     </div>
   );
 }
